3b5b261b455e0f8e66cc776820f10ba690796492
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-05 21:48:23 +0400

    bla

--- src/javaScript/checkoutList.js
+++ src/javaScript/checkoutList.js
@@ -6,20 +6,23 @@ const grandTotal = document.getElementsByClassName("grandTotal")[0];
 const total_p = document.getElementsByClassName("price-TOTAL")[0];
 const vat_p = document.getElementsByClassName("price-VAT")[0];
 const grand_p = document.getElementsByClassName("price-GRAND")[0];
+const shipping_p = document.getElementsByClassName("price-SHIPPING")[0];
 
 let product_type_num = 0;
 let products_num = 0;
 let products_price = 0;
-storageItems?.forEach((info) => {
-  product_type_num++;
-  products_num += +info.quantity;
-  products_price += +info.price.replace(",", "") * +info.quantity;
-});
-total_p.innerHTML = `$ ${products_price.toLocaleString()}`;
-vat_p.innerHTML = `$ ${Math.round(products_price * 0.2).toLocaleString()}`;
-grand_p.innerHTML = `$ ${(products_price + 50).toLocaleString()}`;
 
 if (localStorage.getItem("products")) {
+  storageItems?.forEach((info) => {
+    product_type_num++;
+    products_num += +info.quantity;
+    products_price += +info.price.replace(",", "") * +info.quantity;
+  });
+  total_p.innerHTML = `$ ${products_price.toLocaleString()}`;
+  vat_p.innerHTML = `$ ${Math.round(products_price * 0.2).toLocaleString()}`;
+  grand_p.innerHTML = `$ ${(products_price + 50).toLocaleString()}`;
+  shipping_p.innerHTML = "$ 50";
+
   showArea.innerHTML = "";
   miniShowArea.innerHTML = "";
   for (let i = 0; i < storageItems.length; i++) {
@@ -74,3 +77,13 @@ if (localStorage.getItem("products")) {
   someP.innerHTML = `and ${product_type_num - 1} other item(s)`;
   grandTotal.innerHTML = `$ ${products_price.toLocaleString()}`;
 }
+
+const submiting = document.getElementsByClassName("submit-button")[0];
+submiting.addEventListener("click", () => {
+  [total_p, vat_p, grand_p, shipping_p].forEach((x) => {
+    x.innerHTML = "$ 0";
+  });
+  showArea.innerHTML = "";
+  submiting.disabled = true;
+  submiting.style.cursor = "default";
+});
